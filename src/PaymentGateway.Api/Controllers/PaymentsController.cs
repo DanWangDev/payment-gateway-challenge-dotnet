bb5e57bd83a13c9ff441935e77c862da2453244c
Dan Wang <dan.wang1219@gmail.com>
2026-09-14 11:22:09 +0100

    Chore/ci build (#1)
    * chore: setup github action for the build
    
    * chore: bump up version for .net 8 sdk compatibility
    
    * fix: make the build happy, return 404 when payment is not found

--- src/PaymentGateway.Api/Controllers/PaymentsController.cs
+++ src/PaymentGateway.Api/Controllers/PaymentsController.cs
@@ -21,6 +21,6 @@ public class PaymentsController : Controller
     {
         var payment = _paymentsRepository.Get(id);
 
-        return new OkObjectResult(payment);
+        return payment is null ? NotFound() : new OkObjectResult(payment);
     }
 }
