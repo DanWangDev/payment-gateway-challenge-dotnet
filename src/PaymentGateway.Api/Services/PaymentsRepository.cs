7d3255873679f677c743c0b40e1d97c439b6d063
Dan Wang <dan.wang1219@gmail.com>
2026-09-14 14:58:56 +0100

    refactor: make the payments repository thread-safe

--- src/PaymentGateway.Api/Services/PaymentsRepository.cs
+++ src/PaymentGateway.Api/Services/PaymentsRepository.cs
@@ -1,18 +1,22 @@
-﻿using PaymentGateway.Api.Models.Responses;
+using System.Collections.Concurrent;
+
+using PaymentGateway.Api.Models.Responses;
 
 namespace PaymentGateway.Api.Services;
 
 public class PaymentsRepository
 {
-    public List<PostPaymentResponse> Payments = new();
-    
+    private readonly ConcurrentDictionary<Guid, PostPaymentResponse> _payments = new();
+
+    public IReadOnlyDictionary<Guid, PostPaymentResponse> Payments => _payments;
+
     public void Add(PostPaymentResponse payment)
     {
-        Payments.Add(payment);
+        _payments.TryAdd(payment.Id, payment);
     }
 
-    public PostPaymentResponse Get(Guid id)
+    public PostPaymentResponse? Get(Guid id)
     {
-        return Payments.FirstOrDefault(p => p.Id == id);
+        return _payments.TryGetValue(id, out var payment) ? payment : null;
     }
 }
